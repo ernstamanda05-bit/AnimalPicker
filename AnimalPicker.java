9fb291041ae7a3459a163e7f334b1852ab280674
ernstamanda05-bit <ernstamanda05@gmail.com>
2026-02-20 10:09:02 -0500

    dog or cat

---
+++ AnimalPicker.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Would you like to see a dog or a cat?");
+        String choice = input.nextLine();
+
+        if (choice.equalsIgnoreCase("dog")) {
+            System.out.println("DOG ASCII ART HERE");
+        } else if (choice.equalsIgnoreCase("cat")) {
+            System.out.println("CAT ASCII ART HERE");
+        }
+    }
+}
